6ca01c0e3fda7883146492ee9ac12751f75e3c84
Dukke0 <eriduma@gmail.com>
2022-07-02 13:07:50 +0200

    base explorer

--- src/player/Base.java
+++ src/player/Base.java
@@ -3,13 +3,27 @@ import aic2022.user.*;
 
 public class Base {
 
-    public static void MakeDecision(UnitController uc){
+    public static void MakeDecision(UnitController uc) {
         int randomNumber = (int)(Math.random()*8);
 
-        Direction[] directions = Direction.values();
-        for (Direction dir: directions) {
-            if (uc.canSpawn(UnitType.EXPLORER, dir)) uc.spawn(UnitType.EXPLORER, dir);
+        if (uc.getRound() == 0) {
+            Base.Opening(uc);
+        } else {
+            Direction[] directions = Direction.values();
+
+            for (Direction dir : directions) {
+                if (uc.canSpawn(UnitType.EXPLORER, dir)) uc.spawn(UnitType.EXPLORER, dir);
+            }
         }
+    }
+
+    private static void Opening(UnitController uc) {
+
+        if (uc.canSpawn(UnitType.EXPLORER, Direction.NORTHEAST))
+            uc.spawn(UnitType.EXPLORER, Direction.NORTHEAST);
+
+        if (uc.canSpawn(UnitType.EXPLORER, Direction.SOUTHWEST))
+            uc.spawn(UnitType.EXPLORER, Direction.SOUTHWEST);
     }
 
 }
