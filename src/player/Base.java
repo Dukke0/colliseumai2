2337d30463b31fe923a9b02d4c095b209df76dd2
Dukke0 <eriduma@gmail.com>
2022-07-02 15:18:42 +0200

    dir

--- src/player/Base.java
+++ src/player/Base.java
@@ -12,7 +12,7 @@ public class Base {
             Direction[] directions = Direction.values();
 
             for (Direction dir : directions) {
-                if (uc.canSpawn(UnitType.EXPLORER, dir)) uc.spawn(UnitType.EXPLORER, dir);
+                //if (uc.canSpawn(UnitType.BARBARIAN, dir)) uc.spawn(UnitType.BARBARIAN, dir);
             }
         }
     }
--- src/player/Explorer.java
+++ src/player/Explorer.java
@@ -4,12 +4,35 @@ import aic2022.user.*;
 public class Explorer {
 
     public static void MakeDecision(UnitController uc) {
-        int randomNumber = (int)(Math.random()*8);
+        if (uc.getRound() == 1) {
+            setSpawnDirection(uc, 0, Direction.NORTHEAST);
+            setSpawnDirection(uc, 3, Direction.SOUTHWEST);
+        }
 
-        /*Get corresponding direction*/
-        Direction dir = Direction.values()[randomNumber];
+        if (uc.getInfo().getID() == uc.readOnSharedArray(0)) {
+            moveDirection(uc,0);
+        } else if (uc.getInfo().getID() == uc.readOnSharedArray(3)) {
+            moveDirection(uc,3);
+        }
+    }
 
-        //uc.get
+    private static void moveDirection(UnitController uc, int idx) {
+        int dx = uc.readOnSharedArray(idx + 1);
+        int dy = uc.readOnSharedArray(idx + 2);
+        if (uc.canMove(Direction.getDirection(dx, dy))) uc.move(Direction.getDirection(dx, dy));
+    }
+
+    private static void setSpawnDirection(UnitController uc, int idx, Direction d) {
+        Location loc = new Location(d.dx, d.dy);
+        if (uc.senseUnitAtLocation(loc) == null) {
+            uc.writeOnSharedArray(idx+1, d.dx);
+            uc.writeOnSharedArray(idx+2, d.dy);
+        } else {
+            uc.writeOnSharedArray(idx+1, d.opposite().dx);
+            uc.writeOnSharedArray(idx+2, d.opposite().dy);
+        }
+        uc.writeOnSharedArray(idx, uc.getInfo().getID());
+
 
     }
 
